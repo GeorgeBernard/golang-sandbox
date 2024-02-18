b8bf6e0d9b2d33d625141dc10aaaad35d9fc0985
George Bernard <ghb5@duke.edu>
2024-02-18 21:08:21 +0000

    finish chapter 4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sandbox
 
 go 1.22.0
+
+require github.com/jaymcgavren/car v0.0.0-20190525231050-3dc1df0f9208 // indirect
---
+++ headfirst/4-packages/hi/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"sandbox/headfirst/4-packages/greeting"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
